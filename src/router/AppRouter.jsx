62eef3e79901bbde0f46b50dbac96fd1155cf133
lMarco-Dev <marco.dev2408@gmail.com>
2026-03-11 17:02:37 -0500

    View de catalogo listo!

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Sidebar from "../components/ui/Sidebar"; 
+import Sidebar from "../components/ui/Sidebar";
 import FormularioReportePage from "../pages/publico/FormularioReportePage";
-import ClientesPage from "../pages/admin/ClientesPage"; 
-import ReportesPage from "../pages/admin/ReportesPage"; 
+import ClientesPage from "../pages/admin/ClientesPage";
+import ReportesPage from "../pages/admin/ReportesPage";
 import LoginPage from "../pages/auth/LoginPage";
 import LoginClientePage from "../pages/cliente/LoginClientePage";
-import DashboardPage from "../pages/admin/DashboardPage"; 
+import DashboardPage from "../pages/admin/DashboardPage";
 import DashboardClientePage from "../pages/cliente/DashboardClientePage";
 import { useAuthStore } from "../store/authStore";
+import CatalogosPage from "../pages/admin/CatalogosPage";
 
 export default function AppRouter() {
   const { isAuthenticated, user } = useAuthStore();
 
-  const getDashboardRoute = () => user?.rol === 'CLIENTE' ? '/cliente/dashboard' : '/admin/dashboard';
+  const getDashboardRoute = () =>
+    user?.rol === "CLIENTE" ? "/cliente/dashboard" : "/admin/dashboard";
 
   return (
     <Routes>
@@ -20,22 +22,44 @@ export default function AppRouter() {
       <Route path="/reportar/:token" element={<FormularioReportePage />} />
 
       {/* RUTAS DE LOGIN (Separadas) */}
-      <Route path="/login" element={!isAuthenticated ? <LoginPage /> : <Navigate to={getDashboardRoute()} />} />
-      <Route path="/cliente/login" element={!isAuthenticated ? <LoginClientePage /> : <Navigate to={getDashboardRoute()} />} />
+      <Route
+        path="/login"
+        element={
+          !isAuthenticated ? (
+            <LoginPage />
+          ) : (
+            <Navigate to={getDashboardRoute()} />
+          )
+        }
+      />
+      <Route
+        path="/cliente/login"
+        element={
+          !isAuthenticated ? (
+            <LoginClientePage />
+          ) : (
+            <Navigate to={getDashboardRoute()} />
+          )
+        }
+      />
 
       {/* RUTAS ADMINISTRATIVAS: Solo ADMIN */}
       <Route
         path="/admin/*"
         element={
-          isAuthenticated && user?.rol === 'ADMIN' ? (
+          isAuthenticated && user?.rol === "ADMIN" ? (
             <div className="flex">
-              <Sidebar /> 
+              <Sidebar />
               <main className="flex-1 ml-64 bg-slate-50 min-h-screen">
                 <Routes>
                   <Route path="dashboard" element={<DashboardPage />} />
+                  <Route path="catalogos" element={<CatalogosPage />} />
                   <Route path="clientes" element={<ClientesPage />} />
                   <Route path="reportes" element={<ReportesPage />} />
-                  <Route path="usuarios" element={<div>Gestión de Usuarios (Próximamente)</div>} />
+                  <Route
+                    path="usuarios"
+                    element={<div>Gestión de Usuarios (Próximamente)</div>}
+                  />
                   <Route path="*" element={<Navigate to="dashboard" />} />
                 </Routes>
               </main>
@@ -50,7 +74,7 @@ export default function AppRouter() {
       <Route
         path="/cliente/*"
         element={
-          isAuthenticated && user?.rol === 'CLIENTE' ? (
+          isAuthenticated && user?.rol === "CLIENTE" ? (
             <div className="flex flex-col min-h-screen bg-slate-50">
               <TopbarCliente />
               <main className="flex-1 w-full max-w-7xl mx-auto">
@@ -61,7 +85,9 @@ export default function AppRouter() {
               </main>
             </div>
           ) : (
-            <Navigate to={isAuthenticated ? "/admin/dashboard" : "/cliente/login"} />
+            <Navigate
+              to={isAuthenticated ? "/admin/dashboard" : "/cliente/login"}
+            />
           )
         }
       />
